ae0d800926ffe2e02d238a3f1ca2466e234e9834
gerome12 <gerome.pasquier@gmail.com>
2021-06-09 16:52:50 +0200

    domains not domainEs

--- nginx_certbot_script.sh
+++ nginx_certbot_script.sh
@@ -11,18 +11,18 @@ if [[ ! -f /usr/share/nginx/certificates/fullchain.pem ]];then
     mkdir -p /usr/share/nginx/certificates
 fi
 
-domaines=$(</etc/nginx/domaines)
-first_domaine="$(cut -d',' -f1 <<<$domaines)"
+domains=$(</etc/nginx/domains)
+first_domain="$(cut -d',' -f1 <<<$domains)"
 
 ### If we already have certbot generated certificates, copy them over
-if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$first_domaine/privkey.pem" ]]; then
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$first_domaine/privkey.pem" /usr/share/nginx/certificates/privkey.pem
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$first_domaine/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$first_domaine/chain.pem" /usr/share/nginx/certificates/chain.pem
+if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$first_domain/privkey.pem" ]]; then
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$first_domain/privkey.pem" /usr/share/nginx/certificates/privkey.pem
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$first_domain/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$first_domain/chain.pem" /usr/share/nginx/certificates/chain.pem
 else
     openssl genrsa -out /usr/share/nginx/certificates/privkey.pem 4096
     openssl req -new -key /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/cert.csr -nodes -subj \
-    "/C=PT/ST=World/L=World/O=$first_domaine/OU=kheops lda/CN=${first_domaine}"
+    "/C=PT/ST=World/L=World/O=$first_domain/OU=kheops lda/CN=${first_domain}"
     openssl x509 -req -days 365 -in /usr/share/nginx/certificates/cert.csr -signkey /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/fullchain.pem
     cp /usr/share/nginx/certificates/fullchain.pem /usr/share/nginx/certificates/chain.pem
 fi
